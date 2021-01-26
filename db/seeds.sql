84bd955d6bbdf5c5919ee44aaf484f8e753a48f0
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-25 19:46:44 -0500

    added file structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Beef');
+INSERT INTO burgers (name) VALUES ('Chili Cheese');
+INSERT INTO burgers (name, devoured) VALUES ('Vegan', true);
+INSERT INTO burgers (name, devoured) VALUES ('Blue Cheese', true);
+INSERT INTO burgers (name, devoured) VALUES ('Avocado Lime', true);
+INSERT INTO burgers (name) VALUES ('Triple Stack');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,52 @@
+$(function() {
+  $(".change-devoured").on("click", function(event) {
+    const id = $(this).data("id");
+    const newDevoured = $(this).data("newdevoured");
+
+    const newDevouredState = {
+      devoured: newDevoured
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevoured);
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    const newBurger = {
+      name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    const id = $(this).data("id");
+
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        location.reload();
+      }
+    );
+  });
+});
